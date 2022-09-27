3b8362eedcabc3dba81165600323c148cc4801c4
Daniel Bagley <dseph@users.noreply.github.com>
2022-09-27 19:39:13 -0400

    Add files via upload

---
+++ Test_POP3_OAuth_Delegate.ps1
@@ -0,0 +1,69 @@
+﻿#################################################################################################################################
+# This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment. # 
+# THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,  #
+# INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.               #
+# We grant You a nonexclusive, royalty-free right to use and modify the Sample Code and to reproduce and distribute the object  #
+# code form of the Sample Code, provided that You agree: (i) to not use Our name, logo, or trademarks to market Your software   #
+# product in which the Sample Code is embedded; (ii) to include a valid copyright notice on Your software product in which the  #
+# Sample Code is embedded; and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims   #
+# or lawsuits, including attorneys’ fees, that arise or result from the use or distribution of the Sample Code.                 #
+#################################################################################################################################
+
+#----------------------------------------------------------------------              
+#-     UPDATE VARIABLES TO REFLECT YOUR ENVIRONMENT                   -
+#----------------------------------------------------------------------
+
+# Provide Azure AD Application registration information for your app.
+$AppID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" 
+$TenantId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"  
+
+#----------------------------------------------------------------------              
+#-     DO NOT CHANGE ANY CODE BELOW THIS LINE                         -
+#----------------------------------------------------------------------
+#-                                                                    -
+#-                           Author:  Dirk Buntinx                    -
+#-                           Date:    26/9/2022                       -
+#-                           Version: v1.0                            -
+#-                                                                    -
+#----------------------------------------------------------------------
+
+Write-Host "--------------------"
+Write-Host "- Script settings: -"
+Write-Host "--------------------"
+Write-Host " - AppID: $($AppID)"
+Write-Host " - TenantId: $($TenantId)"
+Write-Host "--------------------"
+Write-Host "-  Start script    -"
+Write-Host "--------------------"
+Write-Host " - Retrieving OAuth token"
+
+# Use the MSAL.PS PowerShell module to get the OAuth token
+# install the MSAL.PS module from here: https://github.com/AzureAD/MSAL.PS
+#Retrieve the MSAL Token
+$Scopes = @("https://outlook.office365.com/POP.AccessAsUser.All")
+$MSALToken = Get-MsalToken -ClientId $AppID -tenantID $TenantId -Scopes $Scopes -Interactive
+
+Write-Host " - Importing PopImap Module"
+# Use the PopImap module to connect to POP3
+# install the PopImap library version 0.1.3 from here: https://www.powershellgallery.com/packages/PopImap/0.1
+# Import the module after installing
+Import-Module PopImap 
+
+
+$Server = "outlook.office365.com"
+$Port = 995
+Write-Host " - Connecting to POP3 server"
+Write-Host " - Server: $($Server)" 
+Write-Host " - Port: $($Port)" 
+Write-Host " - Account: $($MSALToken.Account.UserName)" 
+Write-Host ""
+
+$Pop3 = Get-Pop3Client -Server $Server -Port $Port
+$Pop3.Connect()
+$Pop3.O365Authenticate($MSALToken.AccessToken, $MSALToken.Account.UserName)
+# getting the Statistics for the Mailbox
+$Pop3.ExecuteCommand("STAT")
+$Pop3.Close()
+Write-Host "--------------------"
+Write-Host "-  End script      -"
+Write-Host "--------------------"
